5522afb216aaa4d4be16c2e6415b7abcc11ceec9
LeeHanYeong <dev@azelf.com>
2018-04-09 04:20:38 +0900

    First commit

---
+++ app/config/settings/local.py
@@ -0,0 +1,28 @@
+from .base import *
+
+secrets = json.loads(open(SECRET_LOCAL).read())
+for key, value in secrets.items():
+    setattr(sys.modules[__name__], key, value)
+
+DEBUG = True
+ALLOWED_HOSTS = [
+    '.localhost',
+    '127.0.0.1',
+]
+WSGI_APPLICATION = 'config.wsgi.local.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Static
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(ROOT_DIR, '.media')
